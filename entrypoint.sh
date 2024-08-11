a9ba2a3e37c4d97d79b2528ee6e586194569c0bd
Dishaa01423 <135419958+Dishaa01423@users.noreply.github.com>
2024-08-11 19:26:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Starting Gunicorn..."
-gunicorn --bind 0.0.0.0:$PORT src.main:app --log-level debug
+gunicorn --bind 0.0.0.0:$PORT src.main:app --log-level debug --timeout 120 --workers 1 --threads 2 --reload
