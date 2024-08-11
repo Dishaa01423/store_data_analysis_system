1a769f6ab7a7b8a8e420404106b31b25a57ee69e
Dishaa01423 <135419958+Dishaa01423@users.noreply.github.com>
2024-08-11 18:50:07 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -33,4 +33,5 @@ def api_top_customers():
     return jsonify(top_10_customers.to_dict())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
